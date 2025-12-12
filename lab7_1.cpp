c499c294dd04ce1aa9053ca0337876e206d48d51
balewpewpew <supapitchaitan.2006@gmail.com>
2025-12-12 16:50:05 +0700

    lab7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     int a  =  20, b = 30;
-    float c = 4.0, output;
+    double c = 4.0, output;
 
-    output = (a + b)/c;
+    output = (double)a - b/c;
     cout << "output: " << output << "\n";
     return 0;
 }
